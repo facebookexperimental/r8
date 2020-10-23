c1010ce82af188024283c57c035d386b4cce23c7
Søren Gjesse <sgjesse@google.com>
2020-10-23 14:07:10 +0200

    Version 3.0.0-dev

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -11,7 +11,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "master";
+  public static final String LABEL = "3.0.0-dev";
 
   private Version() {
   }
