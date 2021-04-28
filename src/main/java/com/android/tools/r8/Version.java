e74beb94fc801e73d51e4ac1a024babe917e8902
Morten Krogh-Jespersen <mkroghj@google.com>
2021-04-28 17:41:19 +0200

    Version 3.0.39-dev

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -11,7 +11,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "3.0.38-dev";
+  public static final String LABEL = "3.0.39-dev";
 
   private Version() {
   }
