336787d88762cdc783913f330fe99dfa9ab3e44f
Morten Krogh-Jespersen <mkroghj@google.com>
2021-06-02 11:13:19 +0200

    Only assert puts to final fields outside initializers when testing
    Fixed: 187905573
    Change-Id: I791496a3dee9cff1fb7c017b49d06fd88ad4bb58

--- src/main/java/com/android/tools/r8/graph/AppView.java
+++ src/main/java/com/android/tools/r8/graph/AppView.java
@@ -48,6 +48,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class AppView<T extends AppInfo> implements DexDefinitionSupplier, LibraryModeledPredicate {
 
@@ -714,5 +715,9 @@ public class AppView<T extends AppInfo> implements DexDefinitionSupplier, Librar
     }
     assert alreadyLibraryDesugared != null;
     return alreadyLibraryDesugared.contains(clazz.getType());
+  }
+
+  public boolean checkForTesting(Supplier<Boolean> test) {
+    return testing().enableTestAssertions ? test.get() : true;
   }
 }
