e8f3d73e72a7deda40df0ae9f2467cee7704451b
Ian Zerny <zerny@google.com>
2021-09-21 17:05:12 +0200

    Check all derived class-inlining targets have valid access.
    Bug: 120061431
    Bug: 200057495
    Change-Id: Ic83bcb3d90679160bd4f64252d6dc02fc7faea42

--- src/main/java/com/android/tools/r8/ir/optimize/classinliner/InlineCandidateProcessor.java
+++ src/main/java/com/android/tools/r8/ir/optimize/classinliner/InlineCandidateProcessor.java
@@ -315,10 +315,9 @@ final class InlineCandidateProcessor {
             return user; // Not eligible.
           }
 
-          if (AccessControl.isClassAccessible(singleProgramTarget.getHolder(), method, appView)
-              .isPossiblyFalse()) {
-            return user; // Not eligible.
-          }
+          // The target access is checked in isEligibleSingleTarget above.
+          assert AccessControl.isClassAccessible(singleProgramTarget.getHolder(), method, appView)
+              .isTrue();
 
           // Eligible constructor call (for new instance roots only).
           if (user.isInvokeConstructor(dexItemFactory)) {
@@ -1193,6 +1192,10 @@ final class InlineCandidateProcessor {
       return false;
     }
     if (methodProcessor.isProcessedConcurrently(singleTarget)) {
+      return false;
+    }
+    if (AccessControl.isMemberAccessible(singleTarget, singleTarget.getHolder(), method, appView)
+        .isPossiblyFalse()) {
       return false;
     }
     if (!singleTarget
