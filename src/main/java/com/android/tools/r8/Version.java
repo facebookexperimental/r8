c6b1b96c01e3b102bc3a21395d08f15617a46b43
Christoffer Quist Adamsen <christofferqa@google.com>
2023-02-12 20:13:53 +0100

    Version 8.1.19-dev

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -13,7 +13,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "8.1.18-dev";
+  public static final String LABEL = "8.1.19-dev";
 
   // The prefix of the active dev version line being worked on from this branch. This is used in the
   // few cases where the compiler makes decisions based in the compiler version and where version
